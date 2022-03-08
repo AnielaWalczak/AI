b558c126129d44a014f60bd819533431de53b989
Dominik Jagosz <domjag5@st.amu.edu.pl>
2022-03-08 23:22:51 +0100

    Losowe generowanie poruszających się losowo wózków.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 import pygame
 
-FPS = 60
+FPS = 10
 
 SZEROKOSC_OKNA = 1280
 WYSOKOSC_OKNA = 960
@@ -22,22 +22,27 @@ wozek_ikona = pygame.image.load(os.path.join('Ikony', 'wozek.png'))
 wozek_ze_skrzynka_ikona = pygame.image.load(os.path.join('Ikony', 'wozek_ze_skrzynka.png'))
 agent1 = pygame.transform.scale(pygame.image.load(os.path.join('Ikony', 'wozek_ze_skrzynka.png')),
                                 (BOK_AGENTA1, BOK_AGENTA1))
+lista_agentow = []
+lista_hitboxow_agentow = []
 
 
-def wyswietl_okno(agent1_hitbox):
-    OKNO.fill(BIALY)
-    OKNO.blit(agent1, (agent1_hitbox.x, agent1_hitbox.y))
-    os = random.randint(0, 1)
-    odleglosc = random.randint(20, 120)
-    znak = random.randint(0, 1)
-    if znak == 1:
-        odleglosc = 0 - odleglosc
-    if os == 0:
-        if agent1_hitbox.x + odleglosc < SZEROKOSC_OKNA - agent1_hitbox.width and agent1_hitbox.x + odleglosc > 0:
-            agent1_hitbox.x += odleglosc
-    else:
-        if agent1_hitbox.y + odleglosc < WYSOKOSC_OKNA - agent1_hitbox.height and agent1_hitbox.y + odleglosc > 0:
-            agent1_hitbox.y += odleglosc
+def wyswietl_okno():
+    OKNO.fill(JASNOSZARY1)
+    for a in range(len(lista_agentow)):
+        OKNO.blit(lista_agentow[a], (lista_hitboxow_agentow[a].x, lista_hitboxow_agentow[a].y))
+        os = random.randint(0, 1)
+        odleglosc = random.randint(20, 100)
+        znak = random.randint(0, 1)
+        if znak == 1:
+            odleglosc = 0 - odleglosc
+        if os == 0:
+            if lista_hitboxow_agentow[a].x + odleglosc < SZEROKOSC_OKNA - lista_hitboxow_agentow[a].width and \
+                    lista_hitboxow_agentow[a].x + odleglosc > 0:
+                lista_hitboxow_agentow[a].x += odleglosc
+        else:
+            if lista_hitboxow_agentow[a].y + odleglosc < WYSOKOSC_OKNA - lista_hitboxow_agentow[a].height and \
+                    lista_hitboxow_agentow[a].y + odleglosc > 0:
+                lista_hitboxow_agentow[a].y += odleglosc
     pygame.display.update()
 
 
@@ -54,9 +59,24 @@ def main():
                 warunek_dzialania = False
                 break
 
-        wyswietl_okno(agent1_hitbox)
+        wyswietl_okno()
         print(agent1_hitbox.x, agent1_hitbox.y)
         # wyswietl_agenta1(agent1_hitbox)
+        los = random.randint(1, 30)
+        if len(lista_agentow) < 20:
+            pom = None
+            if los == 1:
+                pom = 'wozek_ze_skrzynka.png'
+            elif los == 2:
+                pom = 'wozek.png'
+            elif los == 3 and len(lista_agentow) > 6:
+                pom = 'traktor_ikona.png'
+            if pom != None:
+                lista_agentow.append(pygame.transform.scale(pygame.image.load(os.path.join('Ikony', pom)),
+                                                            (BOK_AGENTA1, BOK_AGENTA1)))
+                lista_hitboxow_agentow.append(pygame.Rect(random.randint(0, SZEROKOSC_OKNA - BOK_AGENTA1),
+                                                          random.randint(0, WYSOKOSC_OKNA - BOK_AGENTA1), BOK_AGENTA1,
+                                                          BOK_AGENTA1))
 
     pygame.quit()
 
