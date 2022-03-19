71cf238e65ff10f50ee0588c40e8ab711ded68f8
Aniela <walczakaniela116@gmail.com>
2022-03-19 14:46:39 +0100

    m

--- main2.py
+++ main2.py
@@ -1,14 +1,20 @@
 import os
 
 import pygame
-
 from agenci import *
 from stale import *
+import stale
 
 OKNO = pygame.display.set_mode((SZEROKOSC_OKNA, WYSOKOSC_OKNA))
 pygame.display.set_caption("Okno1")
 Agenci1 = []
 
+def kratka(surface):
+    for i in range(stale.NUMBER_OF_BLOCKS_WIDE):
+        new_height=round(i*BLOCK_HEIGHT)
+        new_width =round(i*BLOCK_HEIGHT)
+        pygame.draw.line(surface, BLACK,(0,new_height),(SZEROKOSC_OKNA,new_height),2)
+        pygame.draw.line(surface,BLACK,(new_width,0),(new_width,WYSOKOSC_OKNA),2)
 
 def wyswietl_okno():
     OKNO.fill(JASNOSZARY1)
@@ -18,14 +24,13 @@ def wyswietl_okno():
             a.obierzNowyKierunek()
             a.okreslDlugoscDrogi()
         a.ruszSie(Agenci1)
+        kratka(OKNO)
     pygame.display.update()
 
 
 def dodaj_agenta1():
     los = None
-    if len(Agenci1) in range(0, 40):
-        los = random.randint(1, 150)
-    elif len(Agenci1) in range(40, 50):
+    if len(Agenci1) in range(0, 1):
         los = random.randint(1, 15)
     if los != None:
         pom = None
@@ -48,6 +53,14 @@ def dodaj_agenta1():
                                                (BOK_AGENTA1, BOK_AGENTA1))
                 Agenci1.append(Agent1(hitbox, ikona, KIERUNEK.GORA, 0))
 
+def dodaj_agenta2():
+    los = None
+    if len(Agenci1) in range(0, 1):
+        los = random.randint(1, 15)
+        pom = 'wozek.png'
+        if pom != None:
+            x, y = random.randint(0, SZEROKOSC_OKNA - BOK_AGENTA1), random.randint(0, WYSOKOSC_OKNA - BOK_AGENTA1)
+            hitbox = pygame.Rect(x, y, BOK_AGENTA1, BOK_AGENTA1)
 
 def main():
     klatkaz = pygame.time.Clock()
