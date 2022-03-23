416e35b8b5bd6b0ef3fe8f375d2cdfd3ffd42f4f
s464931 <hannic@st.amu.edu.pl>
2022-03-23 14:59:37 +0100

    inna wersja klas szafka i miejsce

---
+++ miejsce.py
@@ -0,0 +1,10 @@
+class Miejsce:
+    def __init__(self, name, polka, miejsceNaPolce):
+        self.numer = name
+        #self.rozmiar = rozmiar
+        self.polka=polka
+        self.miejsceNaPolce=miejsceNaPolce
+        self.status = 'wolne'
+        self.numerUmieszczonejPaczki=''
+
+
