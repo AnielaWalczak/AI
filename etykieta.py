ffddbbdf0ab2246de74c9a6112d8f1497bbaba85
Aniela Walczak <aniwal4@st.amu.edu.pl>
2022-03-23 16:09:42 +0100

    Zaktualizuj 'etykieta.py'

--- etykieta.py
+++ etykieta.py
@@ -1,5 +1,7 @@
-nazwa_paczki = ""
-producent = ""
-numer_paczki = int
-cecha_paczki = ""
-data_wysylki = ""
+class Etykieta:
+    def __init__(self,nazwa_paczki,producent,numer_paczki,cecha_paczki,data_wysylki):
+        self.nazwa_paczki = nazwa_paczki
+        self.producent=producent
+        self.numer_paczki = numer_paczki
+        self.cecha_paczki = cecha_paczki
+        self.data_wysylki = data_wysylki
